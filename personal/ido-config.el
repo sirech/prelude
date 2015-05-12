e9e6da72d760edf9a25e4e123dddf52c6e1431ec
Mario Fernandez <mario.fernandez@hceris.com>
2015-05-12 23:08:12 +0200

    config for ido

---
+++ personal/ido-config.el
@@ -0,0 +1,13 @@
+;;; ido-config.el --- Configuration for ido.
+
+;;; Comentary:
+
+;; Extra functions and configuration related to ido.
+
+;;; Code:
+
+;; Settings
+(setq ido-use-faces t)
+
+(provide 'ido-config)
+;;; ido-config.el ends here
