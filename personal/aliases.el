ea37dfb7e7ffd097a27bf8e490a6cab4bb523eb3
Mario Fernandez <mario.fernandez@hceris.com>
2015-05-12 23:07:35 +0200

    add aliases

---
+++ personal/aliases.el
@@ -0,0 +1,13 @@
+;;; aliases.el --- Aliases for functions.
+
+;;; Commentary:
+
+;; Provide aliases for some existing functions to use more relevant
+;; names.
+
+;;; Code:
+
+(defalias 'wc 'count-words)
+
+(provide 'aliases)
+;;; aliases.el ends here
