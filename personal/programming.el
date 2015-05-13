0c9558b6c39f93944ef796ac6718f81979f43579
Mario Fernandez <mario.fernandez@xing.com>
2015-05-13 16:51:25 +0200

    Use rbenv

--- personal/programming.el
+++ personal/programming.el
@@ -6,11 +6,20 @@
 
 ;;; Code:
 
+;; JS
+
 (setq js2-basic-offset 2)
 (setq js2-bounce-indent t)
 (setq js2-cleanup-whitespace t)
 (setq js2-highlight-level 3)
 (setq js2-indent-on-enter-key t)
+
+;; Ruby
+(prelude-require-package 'rbenv)
+
+(require 'rbenv)
+(rbenv--setup)
+(rbenv-use-global)
 
 (provide 'programming)
 ;;; programming.el ends here
