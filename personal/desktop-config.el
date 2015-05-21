369dfdb725bb8988a445ae26cd833be6c1f57335
Mario Fernandez <mario.fernandez@xing.com>
2015-05-21 16:56:36 +0200

    Add desktop config

---
+++ personal/desktop-config.el
@@ -0,0 +1,17 @@
+;;; desktop-config.el --- Config for desktop.
+
+;;; Commentary:
+
+;; Save and Restore the session when Emacs is restarted
+
+;;; Code:
+
+(desktop-save-mode 1)
+(defadvice desktop-read (around trace-desktop-errors)
+  (let ((old-debug-on-error debug-on-error))
+    (setq debug-on-error t)
+    ad-do-it
+    (setq debug-on-error old-debug-on-error)))
+
+(provide 'desktop-config)
+;;; desktop-config.el ends here
