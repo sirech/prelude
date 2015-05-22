42111d90cc4c524a849ea337aced0f82ee44d222
Mario Fernandez <mario.fernandez@hceris.com>
2015-05-23 01:54:44 +0200

    add custom config for projectile

---
+++ personal/projectile-config.el
@@ -0,0 +1,13 @@
+;;; projectile-config.el --- Configuration for projectile.
+
+;;; Commentary:
+
+;; Extra functions and configuration related to projectile.
+
+;;; Code:
+
+;; Settings
+(setq projectile-enable-caching t)
+
+(provide 'projectile-config)
+;;; projectile-config.el ends here
