4fe0c05166d14fad25c48befc778c3ff89593a55
Mario Fernandez <mario.fernandez@hceris.com>
2015-05-23 02:25:55 +0200

    add company config

---
+++ personal/company-config.el
@@ -0,0 +1,21 @@
+;;; company-config.el --- Configuration for company.
+
+;;; Commentary:
+
+;; Extra functions and configuration related to company.
+
+;;; Code:
+
+;; Settings
+
+(when (featurep 'prelude-company)
+  (unset-local-key 'smartparens "\M-s")
+  (global-set-key (kbd "M-s") 'company-complete)
+
+  ;; (define-key company-mode-map (kbd "C-n") 'company-select-next)
+  ;; (define-key company-mode-map (kbd "C-p") 'company-select-previous)
+
+  (setq company-idle-delay 1.5))
+
+(provide 'company-config)
+;;; company-config.el ends here
