395924349a5fd968fa10de8d33c99cbf3e41b7e1
Mario Fernandez <sirech@finks.local>
2015-05-24 22:23:30 +0200

    Use helm replacements for chords

--- personal/helm-config.el
+++ personal/helm-config.el
@@ -11,7 +11,11 @@
 (when (featurep 'prelude-helm)
   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
   (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-  (define-key helm-map (kbd "C-z")  'helm-select-action))
+  (define-key helm-map (kbd "C-z")  'helm-select-action)
+
+  (when (featurep 'prelude-key-chord)
+    (key-chord-define-global "yy" 'helm-show-kill-ring)
+    (key-chord-define-global "xx" 'helm-M-x)))
 
 (provide 'helm-config)
 ;;; helm-config.el ends here
