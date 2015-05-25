4684a4fa9f2cb4912adf6387db36ab917599718f
Mario Fernandez <sirech@finks.local>
2015-05-25 13:34:53 +0200

    Add config file for magit

---
+++ personal/magit-config.el
@@ -0,0 +1,12 @@
+;;; magit-config.el --- Configuration for magit.
+
+;;; Commentary:
+
+;; Extra functions and configuration related to magit.
+
+;;; Code:
+
+(setq magit-last-seen-setup-instructions "1.4.0")
+
+(provide 'magit-config)
+;;; magit-config.el ends here
