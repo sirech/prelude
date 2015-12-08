95b25d2c3f0dc80bcf38c91c4f71be090ce93fbe
Mario Fernandez <mario.fernandez@xing.com>
2015-12-08 10:01:01 +0100

    Add config for jsx

---
+++ personal/jsx-config.el
@@ -0,0 +1,16 @@
+;;; jsx-config.el -- Configuration for JSX files (ReactJS).
+
+;; Settings
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
+(prelude-require-package 'jsx-mode)
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
+
+(provide 'jsx-config)
+;;; jsx-config.el ends here
