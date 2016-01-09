ebb8a81015f6a4e3586717628364e704cef7694f
Mario Fernandez <mario.fernandez@xing.com>
2016-01-10 00:53:05 +0100

    slightly bigger font for ls005

--- personal/machines.el
+++ personal/machines.el
@@ -5,7 +5,7 @@
   (set-face-attribute 'default nil :family "Inconsolata" :height 170)
   (prelude-swap-meta-and-super))
  ((equal system-name "ls005")
-  (set-face-attribute 'default nil :family "Inconsolata" :height 150)))
+  (set-face-attribute 'default nil :family "Inconsolata" :height 170)))
 
 (provide 'machines)
 ;;; machines.el ends here
